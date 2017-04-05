eedbcd730b97c773b48d594da7614e125d0e4f8d
Greg Langford <greg.langford@travisperkins.co.uk>
2017-04-05 10:39:21 +0100

    initial commit

---
+++ strtohexstr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *ssid = "this is a long ssid";
+char query[255];
+
+int main(void) {
+  int pos = 0;
+
+  printf("The string is: %s\n", ssid);
+
+  while(*ssid) {
+    char c[2];
+    sprintf(c, "%02X", *ssid++);
+
+    for(int i = 0; i < sizeof(c); i++) {
+      query[pos] = c[i];
+      pos++;
+    }
+  }
+
+  printf("The HEX string is: %s\n", query);
+}
