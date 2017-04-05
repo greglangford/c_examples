b48ead34a40d496a0fa9ee21acd0e9fe18f3917b
Greg Langford <greg.langford@travisperkins.co.uk>
2017-04-05 12:09:22 +0100

    changes

--- strtohexstr.c
+++ strtohexstr.c
@@ -4,23 +4,36 @@
 This snippet will convert the ssid into a HEX string
 */
 
-char *ssid = "this is a long ssid";
+char *ssid = "github.com/greglangford";
 char query[255];
 
-int main(void) {
+/*
+strtohexstr: pass string as first arg and pointer to char array as second arg
+hex encoded string will be writted to the pointer hexstr
+*/
+void strtohexstr(char str[], char *hexstr[]) {
+  char buf[255];
   int pos = 0;
 
-  printf("The string is: %s\n", ssid);
-
-  while(*ssid) {
+  while(*str) {
     char c[2];
-    sprintf(c, "%02X", *ssid++);
+    sprintf(c, "%02X", *str++);
 
     for(int i = 0; i < sizeof(c); i++) {
-      query[pos] = c[i];
+      buf[pos] = c[i];
       pos++;
     }
   }
 
-  printf("The HEX string is: %s\n", query);
+  // copy buf string to hexstr pointer
+  sprintf(*hexstr, "%s", buf);
+}
+
+int main(void) {
+  // create pointer to query
+  char *ptrquery = query;
+
+  printf("Original String: %s\n", ssid);
+  strtohexstr(ssid, &ptrquery);
+  printf("Hex Encoded String: %s\n", query);
 }
